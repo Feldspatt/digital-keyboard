ffeca0b1e00021e7cd6c7fee1507cdc5f0404b72
Mathieu Antoine <mathieuantoine4@gmail.com>
2025-02-26 22:57:28 +0100

    wip

--- src/components/SoundRequest/SoundRequest.tsx
+++ src/components/SoundRequest/SoundRequest.tsx
@@ -1,8 +1,25 @@
 import './SoundRequest.css'
-import * as React from "react";
+import {useEffect, useRef} from "react";
 
 export function SoundRequest() {
-    const [soundAllowed, setSoundAllowed] = React.useState(false);
+    const ref= useRef<HTMLButtonElement>(null);
 
-    return (<>{!soundAllowed ? (<button type="button" onClick={()=>setSoundAllowed(true)} className={"soundAllower"}>Click anywhere to allow sound</button>):''}</>)
+    useEffect(() => {
+        if(ref.current){
+            setTimeout(()=> {
+                if(ref.current) ref.current.style.transform= 'translate3d(-50%,0,0)'
+            },1500)
+        }
+
+        const clickHandler = ()=>{
+            document.removeEventListener('click', clickHandler)
+            if(ref.current){
+                ref.current.style.transform= 'translate3d(-50%, 150%,0)'
+            }
+
+        }
+        document.addEventListener('click', clickHandler)
+    }, []);
+
+    return (<button ref={ref} type="button" className={"soundAllower"}>Click anywhere to allow sound</button>)
 }
